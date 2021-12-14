58bffe8ffbd9aebbc9a77d2482cc1576bfac0055
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2021-12-14 15:55:43 +0100

    one person working

--- Tools/OVDData2FEature/OVD2FeatMain.m
+++ Tools/OVDData2FEature/OVD2FeatMain.m
@@ -14,10 +14,10 @@ AllParticipantsFeat = getDirectoriesWithContentOnly(szAcousticFeatureBaseDir);
 kk = 1; % this will be the outer loop in the end over all participants
 
 for nn = 1:length(AllParticipantsFeat)
-   ind = strfind(AllParticipantsFeat(nn).name,AllParticipantsOVD(kk).name);
-   if ~isempty(ind)
-       break
-   end
+    ind = strfind(AllParticipantsFeat(nn).name,AllParticipantsOVD(kk).name);
+    if ~isempty(ind)
+        break
+    end
 end
 curDirFeat = [AllParticipantsFeat(nn).folder filesep AllParticipantsFeat(nn).name filesep AllParticipantsOVD(kk).name '_AkuData' filesep];
 curDirOVD = [AllParticipantsOVD(kk).folder filesep AllParticipantsOVD(kk).name filesep 'OVD_PredictionResults' filesep];
@@ -44,32 +44,35 @@ end
 
 AllPSDFiles = dir ([curDirFeat 'PSD_*']);
 
+[~,~] = mkdir([curDirFeat 'OVD']); % [~,~] removes warning of existing directory
+
 %nn = length(AllPSDFiles)-1
-nn = 1;
-curPSDFileName =  [AllPSDFiles(nn).folder filesep AllPSDFiles(nn).name];
-
-stInfoFile = GetFeatureFileInfo( curPSDFileName,0);
-idx = find(datetime(stInfoFile.StartTime)==AllOVDTimeVek)
-if (~isempty(idx))
-    OVD_len_1s = 479;
-    OVDexcerpt = AllOVDFeatVek(idx:idx+OVD_len_1s-1);
-
-    if (OVD_len_1s == 479)
-        OVDexcerpt(end+1) = OVDexcerpt(end); % extrapolation for missing block
+for nn = 1:length(AllPSDFiles)
+    curPSDFileName =  [AllPSDFiles(nn).folder filesep AllPSDFiles(nn).name];
+    
+    stInfoFile = GetFeatureFileInfo( curPSDFileName,0);
+    idx = find(datetime(stInfoFile.StartTime)==AllOVDTimeVek);
+    
+    if (~isempty(idx))
+        OVD_len_1s = 479;
+        OVDexcerpt = AllOVDFeatVek(idx:idx+OVD_len_1s-1);
+        
+        if (OVD_len_1s == 479)
+            OVDexcerpt(end+1) = OVDexcerpt(end); % extrapolation for missing block
+        end
+    
+        % filename to save OVD feat Data
+        outname = AllPSDFiles(nn).name;
+        outname(1:3) = 'OVD';
+        szOVD_featFile = [curDirFeat 'OVD' filesep outname];
+        stInfo.fs = stInfoFile.fs;
+        stInfo.HopsizeInSamples = stInfoFile.HopSizeInSamples;
+        stInfo.blockStartTime = datenum(stInfoFile.StartTime);
+        stInfo.FrameSizeInSamples = stInfoFile.FrameSizeInSamples;
+    
+        saveDroidFeatureFile(szOVD_featFile,stInfo,OVDexcerpt);
     end
 end
-[~,~] = mkdir([curDirFeat 'OVD']); % [~,~] removes warning of existing directory
-% filename to save OVD feat Data
-outname = AllPSDFiles(nn).name;
-outname(1:3) = 'OVD';
-szOVD_featFile = [curDirFeat 'OVD' filesep outname];
-stInfo.fs = stInfoFile.fs;
-stInfo.HopsizeInSamples = stInfoFile.HopSizeInSamples;
-stInfo.blockStartTime = datenum(stInfoFile.StartTime);
-stInfo.FrameSizeInSamples = stInfoFile.FrameSizeInSamples;
-
-saveDroidFeatureFile(szOVD_featFile,stInfo,OVDexcerpt);
-
 % and read in again as test
 
 [mFeatureDataNew, mFrameTimeNew, stInfoFileNew ] = LoadFeatureFileDroidAlloc( szOVD_featFile);
